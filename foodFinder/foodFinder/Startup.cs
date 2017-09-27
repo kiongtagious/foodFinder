79fcbaaba561ad4a3f74cbcd03d0f22009372609
kiongtagious <kionglao20@gmai.com>
2017-09-27 15:15:15 -0500

    First commit, testing

---
+++ foodFinder/foodFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(foodFinder.Startup))]
+namespace foodFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
